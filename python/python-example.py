75b4415e8e1023041ac981b589c525b7287f883f
ZhuBinQiang <zhu.binqiang@163.com>
2021-05-28 16:34:26 +0800

    update python-example.py

--- python/python-example.py
+++ python/python-example.py
@@ -8,6 +8,46 @@
 # @description:
 # **********************************************
 
+# refer to https://anandology.com/python-practice-book/iterators.html#the-iteration-protocol
+class Iterator(object):
+
+    def __init__(self, start, stop):
+        self.current = start
+        self.stop =  stop
+
+    def __next__(self):
+        if self.current > self.stop:
+            raise StopIteration
+        else:
+            c = self.current
+            self.current += 1
+        return c
+
+    def __iter__(self):
+        return self
+
+def test_iterator():
+    x = iter([1, 2, 3])
+    print("x1:", next(x))
+
+    count = Iterator(1, 5)
+    print("c1:", next(count))
+
+    print("c2 ~ c5: ", end='')
+    for i in count:
+        print(i, end=',')
+    print()
+
+
+    print("count2: ", end='')
+    count2 = Iterator(5, 10)
+    while True:
+        try:
+            print(next(count2), end=',')
+        except StopIteration:
+            break
+    print()
+
 
 ## refer to https://blog.csdn.net/mieleizhi0522/article/details/82142856
 def yield_statement():
@@ -34,8 +74,29 @@ def test_yield():
     print(g.send(7))
 
 
+## refer to https://github.com/selfteaching/the-craft-of-selfteaching/blob/master/Part.3.B.3.decorator-iterator-generator.ipynb
+## *args 接收并处理所有传递进来的位置参数
+## **kwargs 接收并处理所有传递进来的关键字参数。
+## args: ('Jerry',), kwargs: {'grades': {'Math': 99, 'English': 100, 'Chinese': 100}}
+def a_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("start a_decorator")
+        print(f"calling {func.__name__}, args: {args}, kwargs: {kwargs}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@a_decorator
+def test_decorator(name='Tom', grades={'Math': 100, 'English': 98, 'Chinese': 95}):
+    print(f"Name: {name}")
+    print(f" Math:{grades['Math']}")
+    print(f" English:{grades['English']}")
+    print(f" Chinese:{grades['Chinese']}")
+
+
 def main():
-    test_yield()
+    # test_yield()
+    # test_iterator()
+    test_decorator('Jerry', grades={'Math': 99, 'English': 100, 'Chinese': 100})
 
 if __name__ == '__main__':
     main()
