37412a8b00bbcf9bc3f8ec294d8ad087eb9ba253
ZhuBinQiang <zhu.binqiang@163.com>
2023-07-06 10:52:44 +0800

    add closure in python
    Signed-off-by: ZhuBinQiang <zhu.binqiang@163.com>

--- python/example.py
+++ python/example.py
@@ -1,34 +1,131 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# ********************************************************
-# @file: example.py
+# **********************************************
+# @file: python-example.py
 # @author: ZhuBinQiang <zhu.binqiang@163.com>
-# @create time: 2021-04-06 11:19:13
-# @last modified: 2021-04-06 11:19:13
+# @create time: 2021-04-06 23:28:50
+# @last modified: 2021-04-06 23:28:50
 # @description:
-# ********************************************************
+# **********************************************
 
-import os
-import requests
+# refer to https://anandology.com/python-practice-book/iterators.html#the-iteration-protocol
+class Iterator(object):
 
-def download():
-    url = 'https://example.com/data.tar.gz'
-    user = 'admin'
-    passwd = 'admin'
+    def __init__(self, start, stop):
+        self.current = start
+        self.stop =  stop
 
-    ## https://docs.python-requests.org/zh_CN/latest/api.html#requests.Response.iter_content
-    r = requests.get(url, stream=True, auth=(user, passwd))
-    if r.ok:
-        print("r.ok:%s" %(r.ok))
-        b = True
-        with open(os.path.basename(url), 'wb') as f:
-            for i in r.iter_content(chunk_size=1024):
-                f.write(i)
+    def __next__(self):
+        if self.current > self.stop:
+            raise StopIteration
+        else:
+            c = self.current
+            self.current += 1
+        return c
 
-    session = requests.Session()
-    r2 = session.get(url, stream=True, auth=(user, passwd))
+    def __iter__(self):
+        return self
 
+def handle_iterator():
+    x = iter([1, 2, 3])
+    print("x1:", next(x))
+
+    count = Iterator(1, 5)
+    print("c1:", next(count))
+
+    print("c2 ~ c5: ", end='')
+    for i in count:
+        print(i, end=',')
+    print()
+
+
+    print("count2: ", end='')
+    count2 = Iterator(5, 10)
+    while True:
+        try:
+            print(next(count2), end=',')
+        except StopIteration:
+            break
+    print()
+
+
+## refer to https://blog.csdn.net/mieleizhi0522/article/details/82142856
+def yield_statement():
+    print("Starting ...")
+    while True:
+        res = yield 4
+        print("res:", res)
+
+
+def handle_yield():
+    g = yield_statement()
+    print("00000000000")
+
+    ## 4
+    print(next(g))
+    print("11111111111")
+
+    ## ('res:', None)
+    ## 4
+    print(next(g))
+    print("22222222222")
+
+    ## ('res:', 7)
+    ## 4
+    print(g.send(7))
+
+
+## refer to https://github.com/selfteaching/the-craft-of-selfteaching/blob/master/Part.3.B.3.decorator-iterator-generator.ipynb
+## *args 接收并处理所有传递进来的位置参数
+## **kwargs 接收并处理所有传递进来的关键字参数。
+## args: ('Jerry',), kwargs: {'grades': {'Math': 99, 'English': 100, 'Chinese': 100}}
+def a_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("start a_decorator")
+        print(f"calling {func.__name__}, args: {args}, kwargs: {kwargs}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@a_decorator
+def test_decorator(name='Tom', grades={'Math': 100, 'English': 98, 'Chinese': 95}):
+    print(f"Name: {name}")
+    print(f" Math:{grades['Math']}")
+    print(f" English:{grades['English']}")
+    print(f" Chinese:{grades['Chinese']}")
+
+
+## closure 闭包。简单理解就是
+## 闭包(子函数)是能够读取其他函数(父函数)内部变量的函数
+## 下面用闭包实现一个事件监听器
+## add_listener 和 trigger_event 是闭包，能读取内部变量 listeners
+def event_listener():
+    listeners = []
+
+    def add_listener(listener):
+        listeners.append(listener)
+
+    def trigger_event(data):
+        for listener in listeners:
+            listener(data)
+
+    return (add_listener, trigger_event)
+
+def my_listener(data):
+    print(f"receive data: {data}")
+
+def handle_closure():
+    add_listener, trigger_event = event_listener()
+    add_listener(my_listener)
+    trigger_event("hello")
+
+
+def main():
+    # handle_yield()
+    # handle_iterator()
+    # test_decorator('Jerry', grades={'Math': 99, 'English': 100, 'Chinese': 100})
+    handle_closure()
 
 if __name__ == '__main__':
-    download()
+    main()
+
 
---
+++ python/utils/python-requests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# ********************************************************
+# @file: python-requests.py
+# @author: ZhuBinQiang <zhu.binqiang@163.com>
+# @create time: 2021-04-06 11:19:13
+# @last modified: 2023-07-06 09:00:23
+# @description:
+# ********************************************************
+
+import os
+import requests
+
+def download():
+    url = 'https://example.com/data.tar.gz'
+    user = 'admin'
+    passwd = 'admin'
+
+    ## https://docs.python-requests.org/zh_CN/latest/api.html#requests.Response.iter_content
+    r = requests.get(url, stream=True, auth=(user, passwd))
+    if r.ok:
+        print("r.ok:%s" %(r.ok))
+        b = True
+        with open(os.path.basename(url), 'wb') as f:
+            for i in r.iter_content(chunk_size=1024):
+                f.write(i)
+
+    session = requests.Session()
+    r2 = session.get(url, stream=True, auth=(user, passwd))
+
+
+if __name__ == '__main__':
+    download()
+
