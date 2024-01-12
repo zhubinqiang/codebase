5d0fbb390cfd51e32708e61d6e23e02c1716e846
ZhuBinQiang <zhu.binqiang@163.com>
2024-01-12 17:33:51 +0800

    update func_pointer.c
    Signed-off-by: ZhuBinQiang <zhu.binqiang@163.com>

--- c/func-pointer/func_pointer.c
+++ c/func-pointer/func_pointer.c
@@ -9,12 +9,24 @@ void exampleFunction(int value) {
     printf("Value: %d\n", value);
 }
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     // 声明一个ApiFunction2类型的变量，并将其初始化为指向exampleFunction
     ApiFunction2 funcPtr = exampleFunction;
 
     // 使用函数指针调用函数
     funcPtr(42);  // 输出将会是 "Value: 42"
+
+
+    // 声明函数指针并指向add函数
+    int (*funcPtr2)(int, int) = add;
+
+    // 使用函数指针调用函数
+    int result = funcPtr2(2, 3);
+    printf("Result: %d\n", result);
 
     return 0;
 }
